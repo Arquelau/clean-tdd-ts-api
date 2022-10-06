bdcffb16a9d8fb826692220508bb52c94148d7de
Felipe Arquelau <felipefatp2.0@gmail.com>
2022-10-06 00:33:31 -0300

    fix: change coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*protocols.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
