4acfd550960b7e27bf79d50a0e1caa6a422c2e3e
Felipe Arquelau <felipefatp2.0@gmail.com>
2022-10-25 02:45:08 -0300

    feat: add SaveSurveyResultController

---
+++ src/presentation/controller/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -0,0 +1,21 @@
+import { LoadSurveyById, Controller, HttpRequest, HttpResponse } from './save-survey-result-controller-protocols'
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden, serverError } from '@/presentation/helper/http/http-helper'
+
+export class SaveSurveyResultController implements Controller {
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
